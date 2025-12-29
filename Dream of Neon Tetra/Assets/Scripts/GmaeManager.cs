5d0deb8af9e2e7c345291f9f2df1e63684a6a2db
hiroto iyonaga <k23rs021@st.kyusan-u.ac.jp>
2025-12-29 16:20:10 +0900

    update
    セリフを追加

--- Dream of Neon Tetra/Assets/Scripts/GmaeManager.cs
+++ Dream of Neon Tetra/Assets/Scripts/GmaeManager.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
 namespace NovelGame
 {
     public class GameManager : MonoBehaviour
@@ -11,10 +9,18 @@ namespace NovelGame
         // 別のクラスからGameManagerの変数などを使えるようにするためのもの。（変更はできない）
         public static GameManager Instance { get; private set; }
 
+        public UserScriptManager userScriptManager;
+        public MainTextController mainTextController;
+
+        // ユーザスクリプトの、今の行の数値。クリック（タップ）のたびに1ずつ増える。
+        [System.NonSerialized] public int lineNumber;
+
         void Awake()
         {
             // これで、別のクラスからGameManagerの変数などを使えるようになる。
             Instance = this;
+
+            lineNumber = 0;
         }
     }
 }
---
+++ Dream of Neon Tetra/Assets/Scripts/MainTextController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+namespace NovelGame
+{
+    public class MainTextController : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI _mainTextObject;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            DisplayText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // クリックされたとき、次の行へ移動
+            if (Input.GetMouseButtonUp(0))
+            {
+                GoToTheNextLine();
+                DisplayText();
+            }
+        }
+
+        // 次の行へ移動
+        public void GoToTheNextLine()
+        {
+            GameManager.Instance.lineNumber++;
+        }
+
+        // テキストを表示
+        public void DisplayText()
+        {
+            string sentence = GameManager.Instance.userScriptManager.GetCurrentSentence();
+            _mainTextObject.text = sentence;
+        }
+    }
+}
