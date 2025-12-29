ffddab61be33d524e22d45777538ce63612802df
hiroto iyonaga <k23rs021@st.kyusan-u.ac.jp>
2025-12-30 05:54:47 +0900

    update

--- Dream of Neon Tetra/Assets/Scripts/MainTextController.cs
+++ Dream of Neon Tetra/Assets/Scripts/MainTextController.cs
@@ -8,28 +8,44 @@ namespace NovelGame
     public class MainTextController : MonoBehaviour
     {
         [SerializeField] TextMeshProUGUI _mainTextObject;
+        [SerializeField] float _textSpeed = 0.05f; // 文字を表示する速さ
 
-        // Start is called before the first frame update
+        private bool _isTyping = false; // 現在文字を表示中か
+        private string _currentSentence = ""; // 現在表示すべき全文
+
         void Start()
         {
-            // 最初の行のテキストを表示、または命令を実行
-            string statement = GameManager.Instance.userScriptManager.GetCurrentSentence();
-            if (GameManager.Instance.userScriptManager.IsStatement(statement))
+            // 最初の行を取得
+            _currentSentence = GameManager.Instance.userScriptManager.GetCurrentSentence();
+
+            // 命令文（&）なら実行して次の行へ
+            if (GameManager.Instance.userScriptManager.IsStatement(_currentSentence))
             {
-                GameManager.Instance.userScriptManager.ExecuteStatement(statement);
-                GoToTheNextLine();
+                HandleStatement();
             }
-            DisplayText();
+            else
+            {
+                StartCoroutine(TypeText());
+            }
         }
 
-        // Update is called once per frame
         void Update()
         {
-            // クリックされたとき、次の行へ移動
+            // クリックされたときの処理
             if (Input.GetMouseButtonUp(0))
             {
-                GoToTheNextLine();
-                DisplayText();
+                if (_isTyping)
+                {
+                    // タイピング中にクリック：一瞬で全文表示
+                    StopAllCoroutines();
+                    _mainTextObject.text = _currentSentence;
+                    _isTyping = false;
+                }
+                else
+                {
+                    // 表示完了後にクリック：次の行へ
+                    GoToTheNextLine();
+                }
             }
         }
 
@@ -37,19 +53,48 @@ namespace NovelGame
         public void GoToTheNextLine()
         {
             GameManager.Instance.lineNumber++;
-            string sentence = GameManager.Instance.userScriptManager.GetCurrentSentence();
-            if (GameManager.Instance.userScriptManager.IsStatement(sentence))
+            _currentSentence = GameManager.Instance.userScriptManager.GetCurrentSentence();
+
+            // 文がなくなったら終了（エラー防止）
+            if (string.IsNullOrEmpty(_currentSentence)) return;
+
+            if (GameManager.Instance.userScriptManager.IsStatement(_currentSentence))
             {
-                GameManager.Instance.userScriptManager.ExecuteStatement(sentence);
-                GoToTheNextLine();
+                HandleStatement();
+            }
+            else
+            {
+                DisplayText();
             }
         }
 
-        // テキストを表示
+        // 命令文を再帰的に処理する
+        private void HandleStatement()
+        {
+            GameManager.Instance.userScriptManager.ExecuteStatement(_currentSentence);
+            GoToTheNextLine();
+        }
+
+        // テキスト表示開始
         public void DisplayText()
         {
-            string sentence = GameManager.Instance.userScriptManager.GetCurrentSentence();
-            _mainTextObject.text = sentence;
+            StopAllCoroutines();
+            StartCoroutine(TypeText());
+        }
+
+        // 一文字ずつ表示するコルーチン
+        IEnumerator TypeText()
+        {
+            _isTyping = true;
+            _mainTextObject.text = "";
+
+            foreach (char c in _currentSentence)
+            {
+                _mainTextObject.text += c;
+                yield return new WaitForSeconds(_textSpeed);
+            }
+
+            _isTyping = false;
         }
     }
 }
--- Dream of Neon Tetra/Assets/Scripts/UserScriptManager.cs
+++ Dream of Neon Tetra/Assets/Scripts/UserScriptManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.SceneManagement; // シーン遷移に必要
 
 namespace NovelGame
 {
     public class UserScriptManager : MonoBehaviour
     {
         [SerializeField] TextAsset _textFile;
+        [SerializeField] string _nextSceneName; // インスペクターで遷移先のシーン名を入力
 
         // 文章中の文（ここでは１行ごと）を入れておくためのリスト
         List<string> _sentences = new List<string>();
@@ -19,6 +21,7 @@ namespace NovelGame
             while (reader.Peek() != -1)
             {
                 string line = reader.ReadLine();
+                // 空行を無視したい場合は if (!string.IsNullOrWhiteSpace(line)) を使う
                 _sentences.Add(line);
             }
         }
@@ -26,12 +29,36 @@ namespace NovelGame
         // 現在の行の文を取得する
         public string GetCurrentSentence()
         {
+            // インデックスがリストの範囲外（全ての行を読み終えた後）かチェック
+            if (GameManager.Instance.lineNumber >= _sentences.Count)
+            {
+                // 全て読み終えていたらシーン遷移を実行
+                TransitionToNextScene();
+                return null;
+            }
+
             return _sentences[GameManager.Instance.lineNumber];
+        }
+
+        // シーン遷移の実行
+        private void TransitionToNextScene()
+        {
+            if (!string.IsNullOrEmpty(_nextSceneName))
+            {
+                SceneManager.LoadScene(_nextSceneName);
+            }
+            else
+            {
+                Debug.LogWarning("次のシーン名が設定されていません。");
+            }
         }
 
         // 文が命令かどうか
         public bool IsStatement(string sentence)
         {
+            // sentence が null の場合のチェックを追加（遷移時のエラー防止）
+            if (string.IsNullOrEmpty(sentence)) return false;
+
             if (sentence[0] == '&')
             {
                 return true;
@@ -42,6 +69,8 @@ namespace NovelGame
         // 命令を実行する
         public void ExecuteStatement(string sentence)
         {
+            if (string.IsNullOrEmpty(sentence)) return;
+
             string[] words = sentence.Split(' ');
             switch (words[0])
             {
